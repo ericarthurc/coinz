4011d7043751a891e3fa9daa57d0084fdfdfe8a3
Eric Christensen <hebrew.8.12@gmail.com>
2025-02-10 07:49:19 -0800

    07:49 02/10/25

--- frontend/src/components/ExpenseCard.tsx
+++ frontend/src/components/ExpenseCard.tsx
@@ -3,12 +3,16 @@ import { Component } from "solid-js";
 const ExpenseCard: Component<{ expense: any }> = (props) => {
   return (
     <div class="expense_card">
-      <span>
-        {`${new Date(props.expense.timestamp).toLocaleDateString()}`} |{" "}
+      <span class="expense_card_date">
+        {`${new Date(props.expense.timestamp).toLocaleDateString()}`}
       </span>
-      <span>{props.expense.store} | </span>
-      <span class="amount">${props.expense.amount}</span>
-      <button>x</button>
+
+      <span class="expense_card_store">{props.expense.store}</span>
+
+      <div class="expense_card_box_3">
+        <span class="expense_card_amount">${props.expense.amount}</span>
+        <button class="expense_card_button_add">x</button>
+      </div>
     </div>
   );
 };
